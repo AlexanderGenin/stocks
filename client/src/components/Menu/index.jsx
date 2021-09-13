b1ec0a111d406ff7c13df3177ff0e59edebeaf32
Alex <55929549+AlexanderGenin@users.noreply.github.com>
2021-09-13 17:54:39 +0300

    Edit Menu styles

--- client/src/components/Menu/index.jsx
+++ client/src/components/Menu/index.jsx
@@ -1,32 +1,48 @@
 import React, { Component } from "react";
 import { Logo, Sidebar, Author, Social } from "./sidebar-styles";
-import { IoLogoInstagram, IoLogoLinkedin, IoLogoGithub } from "react-icons/io";
+import { FaFacebookF, FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import Nav from "../Nav";
 
 export class Menu extends Component {
   render() {
+    const { items, onItemSelect, selectedItem } = this.props;
+
     return (
       <Sidebar>
         <Logo>Stocksy</Logo>
         <Nav
-          items={this.props.items}
-          onItemSelect={this.props.onItemSelect}
-          selectedItem={this.props.selectedItem}
+          items={items}
+          onItemSelect={onItemSelect}
+          selectedItem={selectedItem}
         />
         <Author>
-          <IconContext.Provider value={{ size: "1.4rem" }}>
+          <IconContext.Provider value={{ size: "1.2rem" }}>
             <p>By Alex Genin</p>
-            <a href="alexgenin.com">alexgenin.com</a>
+            <a href="https://alexgenin.com" target="_blank" rel="noreferrer">
+              alexgenin.com
+            </a>
             <Social>
-              <a href="#">
-                <IoLogoLinkedin />
+              <a
+                href="https://www.linkedin.com/in/alex-genin/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedinIn />
               </a>
-              <a href="#">
-                <IoLogoGithub />
+              <a
+                href="https://github.com/alexandergenin"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub />
               </a>
-              <a href="#">
-                <IoLogoInstagram />
+              <a
+                href="https://www.facebook.com/alexgenin99"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaFacebookF />
               </a>
             </Social>
           </IconContext.Provider>
